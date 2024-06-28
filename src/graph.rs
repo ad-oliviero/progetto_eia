cfecba29fdf9c3402c829dddbbe425cfda70930a
Adriano Oliviero <adrianoliviero23@gmail.com>
2024-06-28 15:50:07 +0200

    Refactored some data structures, added a smaller dataset

--- src/graph.rs
+++ src/graph.rs
@@ -1,135 +1,147 @@
 pub mod graph {
     use flate2::read::GzDecoder;
-    use std::collections::{HashMap, HashSet};
+    use std::collections::{BTreeMap, BTreeSet, VecDeque};
     use std::fs::File;
     use std::io::{BufRead, BufReader};
+    use std::mem;
 
     use timed_run;
 
+    pub type State = u32;
+
     #[derive(Clone)]
     pub struct Problem {
-        start: u32,
-        end: u32,
+        start: State,
+        end: State,
     }
 
     impl Problem {
         pub fn new() -> Problem {
             Problem { start: 0, end: 0 }
         }
-        pub fn set_problem(&mut self, start: u32, end: u32) {
+        pub fn set_problem(&mut self, start: State, end: State) {
             self.start = start;
             self.end = end;
         }
-        pub fn get_start(&self) -> u32 {
+        pub fn get_start(&self) -> State {
             self.start
         }
-        pub fn get_end(&self) -> u32 {
+        pub fn get_end(&self) -> State {
             self.end
         }
-        pub fn is_goal(&self, node: u32) -> bool {
+        pub fn is_goal(&self, node: State) -> bool {
             node == self.end
         }
     }
 
     pub struct Graph {
-        links: HashMap<u32, HashSet<u32>>,
+        nodes: BTreeMap<State, BTreeSet<State>>,
+        edge_count: u32,
         problem: Problem,
     }
 
     impl Graph {
         pub fn new() -> Graph {
             Graph {
-                links: HashMap::new(),
+                nodes: BTreeMap::new(),
+                edge_count: 0,
                 problem: Problem::new(),
             }
         }
-        pub fn set_search_problem(&mut self, from: u32, to: u32) {
-            if !self.links.contains_key(&from) || !self.links.contains_key(&to) {
-                panic!("The nodes {} and {} are not in the dataset", from, to);
-            }
-            if from == to {
-                panic!("The nodes {} and {} are the same", from, to);
+        pub fn from_file(dataset_path: &str) -> Graph {
+            let mut graph = Graph::new();
+            graph.load_dataset(dataset_path);
+            graph
+        }
+        pub fn set_search_problem(&mut self, from: State, to: State) {
+            if from >= self.nodes.len() as State || to >= self.nodes.len() as State {
+                panic!("Invalid search problem");
             }
             self.problem.set_problem(from, to);
         }
         pub fn get_problem(&self) -> Problem {
             self.problem.clone()
         }
-        pub fn get_links(&self) -> HashMap<u32, HashSet<u32>> {
-            self.links.clone()
-        }
-        pub fn load_dataset(&mut self) {
-            timed_run!({
+        pub fn load_dataset(&mut self, dataset_path: &str) {
+            let elapsed = timed_run!({
                 // read the dataset file
-                let lines =
-                    BufReader::new(GzDecoder::new(File::open("roadNet-CA.txt.gz").unwrap()))
-                        .lines()
-                        .map(|line| line.unwrap());
+                let lines = BufReader::new(GzDecoder::new(File::open(dataset_path).unwrap()))
+                    .lines()
+                    .map(|line| line.unwrap());
 
-                // Load every line into the HashMap
+                // Load every line into the BTree
                 for line in lines {
                     let mut iter = line.split_whitespace();
-
-                    if let (Some(node_l), Some(node_r)) = (iter.next(), iter.next()) {
-                        if let (Ok(node_l), Ok(node_r)) = (node_l.parse(), node_r.parse()) {
-                            // get the entry of the node_l
-                            self.links
-                                .entry(node_l)
-                                // if it does not exist, create a new one
+                    if let (Some(from), Some(to)) = (iter.next(), iter.next()) {
+                        if let (Ok(from), Ok(to)) = (from.parse::<State>(), to.parse::<State>()) {
+                            /*
+                             * if both insertions are successful, it means
+                             * that the connection did not exist before
+                             */
+                            self.edge_count += (self
+                                // get the entry of the node_l
+                                .nodes
+                                .entry(from.clone())
+                                // if it does not exist, create a new BTreeSet (the default)
                                 .or_default()
-                                // insert node_r into the HashSet, avoiding duplicates
-                                .insert(node_r);
+                                // then insert the node_r
+                                .insert(to.clone())
+                                // the same for node_r entry
+                                && self.nodes.entry(to).or_default().insert(from))
+                                as u32; // it is a boolean, so cast it to u32 and we get either 1 or 0
                         }
                     }
                 }
-                println!("Loaded {} nodes", self.links.len());
             });
+            println!("Loaded in {:.3}s", elapsed.as_secs_f64());
+            println!("Loaded {} nodes", self.nodes.len());
+            println!("Loaded {} edges", self.edge_count);
         }
-        pub fn best_first_search(&mut self) -> Option<Vec<u32>> {
-            let mut nodo: u32 = self.problem.get_start();
-            let mut path: Vec<u32> = Vec::new();
-            let mut fringe: Vec<u32> = Vec::new();
-            let mut visited: HashSet<u32> = HashSet::new();
-            path.push(nodo);
-            fringe.push(nodo);
-            visited.insert(nodo);
-            while !fringe.is_empty() {
-                nodo = fringe.pop().unwrap();
-                path.push(nodo);
-                if self.problem.is_goal(nodo) {
-                    return Some(path);
-                }
-                for child in self.links.get(&nodo).unwrap() {
-                    if !visited.contains(child) {
-                        visited.insert(*child);
-                        fringe.push(*child);
-                    }
+        pub fn estimate_dataset_size(&self) -> usize {
+            let mut size = 0;
+            for (node, links) in &self.nodes {
+                size += mem::size_of_val(node);
+                size += mem::size_of_val(links);
+                for link in links {
+                    size += mem::size_of_val(link);
+                    size += mem::size_of_val(&self.nodes[link]);
                 }
             }
+            size
+        }
+        pub fn best_first_search(&mut self) -> Option<Vec<State>> {
+            let mut nodo: State = self.problem.get_start();
+            let mut path: Vec<State> = Vec::new();
+            let mut fringe: Vec<State> = Vec::new();
+            let mut visited: Vec<State> = Vec::new();
             return None;
         }
-        pub fn breadth_first_search(&mut self) -> Option<Vec<u32>> {
+        pub fn breadth_first_search(&mut self) -> Option<Vec<State>> {
+            let mut nodo: State = self.problem.get_start();
+            let mut path: Vec<State> = Vec::new();
+            let mut fringe: VecDeque<State> = VecDeque::new(); // fifo queue
+            let mut visited: Vec<State> = Vec::new();
             return None;
         }
-        pub fn uniform_cost_search(&mut self) -> Option<Vec<u32>> {
+        pub fn uniform_cost_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
-        pub fn depth_first_search(&mut self) -> Option<Vec<u32>> {
+        pub fn depth_first_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
-        pub fn depth_limited_search(&mut self) -> Option<Vec<u32>> {
+        pub fn depth_limited_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
-        pub fn iterative_deepening_search(&mut self) -> Option<Vec<u32>> {
+        pub fn iterative_deepening_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
-        pub fn bi_directional_search(&mut self) -> Option<Vec<u32>> {
+        pub fn bi_directional_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
-        pub fn greedy_search(&mut self) -> Option<Vec<u32>> {
+        pub fn greedy_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
-        pub fn a_star_search(&mut self) -> Option<Vec<u32>> {
+        pub fn a_star_search(&mut self) -> Option<Vec<State>> {
             return None;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
 extern crate flate2;
-extern crate heapsize;
 extern crate rand;
 mod graph;
 mod macros;
 
+use std::thread::sleep;
+
+// use heapsize::HeapSizeOf;
 use graph::graph::*;
-use heapsize::HeapSizeOf;
 
 fn test_search_function(
     graph: &mut Graph,
     name: &str,
-    search_function: fn(&mut Graph) -> Option<Vec<u32>>,
+    search_function: fn(&mut Graph) -> Option<Vec<State>>,
 ) {
     let mut path_len = 0;
     let mut found = false;
-    timed_run!({
+    let elapsed = timed_run!({
         if let Some(path) = search_function(graph) {
             path_len = path.len();
             found = true;
         }
-        println!(
-            "{:<20} | {:<20} | {:10}",
+        print!(
+            "{:<20} | {:<20} | {:>11} |",
             name,
             if found { "Success" } else { "Failure" },
             if found { path_len } else { 0 },
         );
     });
+    println!(" {:>7}.{:03}s", elapsed.as_secs(), elapsed.subsec_millis());
 }
 
 #[rustfmt::skip]
 fn test_all_search_functions(graph: &mut Graph) {
+    println!("\x1b[1m{:^20} | {:^20} | {:^11} | {:^10}\x1b[0m",
+             "Algorithm", "Result", "Path Length", "Elapsed Time");
     test_search_function(graph, "BestFirst", Graph::best_first_search);
     test_search_function(graph, "BreadthFirst", Graph::breadth_first_search);
     test_search_function(graph, "UniformCost", Graph::uniform_cost_search);
@@ -42,31 +46,32 @@ fn test_all_search_functions(graph: &mut Graph) {
 }
 
 fn main() {
-    let mut graph = Graph::new();
-    graph.load_dataset();
+    let mut graph = Graph::from_file("roadNet-CA.txt.gz");
+    let dataset_size = graph.estimate_dataset_size();
     println!(
-        "The dataset takes {:.3}Mb of memory",
-        graph.get_links().heap_size_of_children() as f64 / (1024.0 * 1024.0)
+        "The dataset takes ~{} of memory",
+        match dataset_size {
+            size if size < 1024 * 9 => format!("{}B", size),
+            size if size < 1024 * 1024 * 9 => format!("{}KB", size / 1024),
+            size => format!("{} MB", size / (1024 * 1024)),
+        }
     );
     // set random values to test the search algorithms
-    let random_node = |max: u32| -> u32 { rand::random::<u32>() % max };
-    let random_from = random_node(graph.get_links().len() as u32);
-    let random_to = {
-        let mut v = random_node(graph.get_links().len() as u32);
-        while v == random_from {
-            v = random_node(graph.get_links().len() as u32);
-        }
-        v
-    };
-    graph.set_search_problem(random_from, random_to);
+    // let random_node = |max: u32| -> u32 { rand::random::<u32>() % max };
+    // let random_from = random_node(graph.get_links().len() as u32);
+    // let random_to = {
+    //     let mut v = random_node(graph.get_links().len() as u32);
+    //     while v == random_from {
+    //         v = random_node(graph.get_links().len() as u32);
+    //     }
+    //     v
+    // };
+    // graph.set_search_problem(random_from, random_to);
+    graph.set_search_problem(0, 1);
     println!(
         "Starting search from: {} to: {}",
         graph.get_problem().get_start(),
         graph.get_problem().get_end()
-    );
-    println!(
-        "\x1b[1m{:<20} | {:<20} | {:10}\x1b[0m",
-        "Algorithm", "Result", "Path Length"
     );
 
     test_all_search_functions(&mut graph);
